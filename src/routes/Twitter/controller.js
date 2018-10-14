0e5d48b7fa3a1a2d4275638b47eef6818589b00a
numano <mselmany@Muhammeds-MacBook-Pro.local>
2018-10-15 01:12:02 +0300

    @Twitter parse işlemleri yapılıyor; - quoted(alıntılanmış tweetler yapılacak).

--- src/routes/Twitter/controller.js
+++ src/routes/Twitter/controller.js
@@ -13,9 +13,7 @@ class Twitter extends ApiBase {
       init: { username, consumer_key, consumer_secret }
     });
     this.username = username;
-    this.credentials = new Buffer(
-      `${consumer_key}:${consumer_secret}`
-    ).toString("base64");
+    this.credentials = new Buffer(`${consumer_key}:${consumer_secret}`).toString("base64");
   }
 
   static parser({ name, type, form }, payload) {
@@ -23,193 +21,169 @@ class Twitter extends ApiBase {
       return [];
     }
     switch (type) {
-      case "timelines": {
-        let {
-          id_str,
-          created_at,
-          text,
-          retweet_count,
-          favorite_count,
-          favorited,
-          retweeted,
-          entities,
-          user
-        } = payload;
-
-        let _user;
-        {
-          _user = {
-            id_str,
-            name,
-            screen_name,
-            location,
-            description,
-            url,
-            entities,
-            followers_count,
-            friends_count,
-            favourites_count,
-            verified,
-            statuses_count,
-            lang,
-            profile_banner_url,
-            profile_image_url_https
-          } = user;
-        }
-
-        return {
-          __source: { name, type, form },
-          id: id_str,
-          created_at,
-          text,
-          retweet_count,
-          favorite_count,
-          favorited,
-          retweeted,
-          entities,
-          user: _user
-        };
-      }
-
-      case "comments": {
-        return payload.collection.map(item => {
-          const {
-            id,
-            kind,
-            created_at,
-            user_id,
-            track_id,
-            timestamp,
-            body
-          } = item;
-
+      case "timeline": {
+        return payload.map(item => {
           return {
             __source: { name, type, form },
-            id,
-            kind,
-            created_at: new Date(created_at).getTime(),
-            user_id,
-            track_id,
-            timestamp,
-            body
+            ...generateItem(item)
           };
         });
-      }
-
-      case "favorites":
-      case "tracks": {
-        return payload.collection.map(item => {
-          const {
-            id,
-            kind,
-            created_at,
-            user_id,
-            duration,
-            tag_list,
-            permalink_url,
-            artwork_url,
-            waveform_url,
-            stream_url,
-            playback_count,
-            favoritings_count,
-            comment_count,
-            purchase_url,
-            genre,
-            title,
-            description,
-            label_name,
-            original_format,
-            user
-          } = item;
-
-          return {
-            __source: { name, type, form },
-            id,
-            kind,
-            created_at: new Date(created_at).getTime(),
-            user_id,
-            duration,
-            tag_list,
-            permalink_url,
-            artwork_url,
-            waveform_url,
-            stream_url,
-            playback_count,
-            favoritings_count,
-            comment_count,
-            purchase_url,
-            genre,
-            title,
-            description,
-            label_name,
-            original_format,
-            user: {
-              id: user.id,
-              username: user.username,
-              permalink_url: user.permalink_url,
-              avatar_url: user.avatar_url
-            }
-          };
-        });
-      }
-
-      case "track": {
-        const {
-          id,
-          kind,
-          created_at,
-          user_id,
-          duration,
-          tag_list,
-          permalink_url,
-          artwork_url,
-          waveform_url,
-          stream_url,
-          playback_count,
-          favoritings_count,
-          comment_count,
-          purchase_url,
-          genre,
-          title,
-          description,
-          label_name,
-          original_format,
-          user
-        } = payload;
-
-        return {
-          __source: { name, type, form },
-          id,
-          kind,
-          created_at: new Date(created_at).getTime(),
-          user_id,
-          duration,
-          tag_list,
-          permalink_url,
-          artwork_url,
-          waveform_url,
-          stream_url,
-          playback_count,
-          favoritings_count,
-          comment_count,
-          purchase_url,
-          genre,
-          title,
-          description,
-          label_name,
-          original_format,
-          user: {
-            id: user.id,
-            username: user.username,
-            permalink_url: user.permalink_url,
-            avatar_url: user.avatar_url
-          }
-        };
       }
 
       default: {
         return payload;
       }
+    }
+
+    function generateItem({
+      id_str,
+      created_at,
+      recreated_at = created_at,
+      retweet_count,
+      favorite_count,
+      owner = true,
+      user,
+      retweeted_status = null,
+      entities,
+      extended_entities = null,
+      text
+    } = {}) {
+      if (retweeted_status) {
+        return generateItem({
+          ...retweeted_status,
+          owner: false,
+          recreated_at: created_at
+        });
+      } else {
+        let media;
+        if (extended_entities) {
+          entities = { ...entities, ...extended_entities };
+        }
+
+        if (entities.user_mentions && entities.user_mentions.length) {
+          entities.user_mentions.forEach(({ screen_name, name }) => {
+            text = text.replace(
+              `@${screen_name}`,
+              `<a href="https://twitter.com/${screen_name}" title="${name}">@${screen_name}</a>`
+            );
+          });
+        }
+
+        // search its last url index (for remove or not)
+        const tweet_url_idx = text.lastIndexOf("https://t.co/");
+        // if it exist
+        if (tweet_url_idx > -1) {
+          // extract it
+          const tweet_url = text.substring(tweet_url_idx);
+          // if tweet has url list
+          if (entities.urls && entities.urls.length) {
+            // then if it is same any of the url list
+            const is_needed_url_exist = entities.urls.filter(({ url }) => tweet_url.includes(url));
+            // then this means, it is not a tweet url(it is a some content url, exp: Soundcloud, etc.), so do not remove from "text"
+            if (!is_needed_url_exist.length) {
+              text = text.substring(0, tweet_url_idx - 1);
+            }
+          } else {
+            text = text.substring(0, tweet_url_idx - 1);
+          }
+        }
+
+        if (entities.urls && entities.urls.length) {
+          // wrap urls with <a>
+          entities.urls.forEach(({ url, expanded_url, display_url }) => {
+            text = text.replace(url, `<a href="${expanded_url}">${display_url}</a>`);
+          });
+        }
+
+        if (entities.media && entities.media.length) {
+          media = entities.media.map(
+            ({ id_str, media_url_https, url, display_url, type, video_info }) => {
+              if (video_info) {
+                let { aspect_ratio, duration_millis, variants } = video_info;
+
+                video_info = {
+                  aspect_ratio,
+                  duration_millis,
+                  source: variants
+                    .filter(item => item.hasOwnProperty("bitrate"))
+                    .sort((a, b) => b.bitrate - a.bitrate)[0]
+                };
+              }
+              return {
+                id_str,
+                media_url_https,
+                url,
+                display_url,
+                type,
+                video_info
+              };
+            }
+          );
+        }
+
+        return {
+          id_str,
+          owner,
+          created_at,
+          recreated_at,
+          tweet_url: `https://twitter.com/${user.screen_name}/status/${id_str}`,
+          text,
+          media,
+          retweet_count,
+          favorite_count,
+          user: generateUser(user)
+        };
+      }
+    }
+
+    function generateUser({
+      id_str,
+      name,
+      screen_name,
+      location,
+      followers_count,
+      friends_count,
+      listed_count,
+      created_at,
+      favourites_count,
+      verified,
+      statuses_count,
+      lang,
+      profile_image_url_https,
+      profile_banner_url,
+      description,
+      url,
+      entities
+    } = {}) {
+      if (url && entities.url.urls.length) {
+        const _urls = entities.url.urls[0];
+        url = `<a href="${_urls.expanded_url}">${_urls.display_url}</a>`;
+      }
+
+      if (description && entities.description.urls.length) {
+        entities.description.urls.forEach(({ url, expanded_url, display_url }) => {
+          description = description.replace(url, `<a href="${expanded_url}">${display_url}</a>`);
+        });
+      }
+
+      return {
+        id_str,
+        name,
+        screen_name,
+        location,
+        followers_count,
+        friends_count,
+        listed_count,
+        created_at,
+        favourites_count,
+        verified,
+        statuses_count,
+        lang,
+        profile_image_url_https,
+        profile_banner_url,
+        url,
+        description
+      };
     }
   }
 
@@ -222,17 +196,13 @@ class Twitter extends ApiBase {
         data: this.messages.ALREADY_EXIST
       };
     }
-    const r = await this.client.post(
-      "/token",
-      "grant_type=client_credentials",
-      {
-        baseURL: AUTH_URL,
-        headers: {
-          Authorization: `Basic ${this.credentials}`,
-          "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
-        }
+    const r = await this.client.post("/token", "grant_type=client_credentials", {
+      baseURL: AUTH_URL,
+      headers: {
+        Authorization: `Basic ${this.credentials}`,
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
       }
-    );
+    });
     const { token_type, access_token } = r.data;
     this.authorization = `${token_type} ${access_token}`;
     return { success: true, source, data: this.messages.ACCESS_GRANTED };
@@ -248,17 +218,13 @@ class Twitter extends ApiBase {
       return { success: false, source, data: this.messages.NOT_AUTHORIZED };
     }
     this.required({ access_token });
-    await this.client.post(
-      "/invalidate_token",
-      `access_token=${access_token}`,
-      {
-        baseURL: AUTH_URL,
-        headers: {
-          Authorization: `Basic ${this.credentials}`,
-          "Content-Type": "application/x-www-form-urlencoded;"
-        }
+    await this.client.post("/invalidate_token", `access_token=${access_token}`, {
+      baseURL: AUTH_URL,
+      headers: {
+        Authorization: `Basic ${this.credentials}`,
+        "Content-Type": "application/x-www-form-urlencoded;"
       }
-    );
+    });
     await this.token();
     return { success: true, source, data: this.messages.ACCESS_GRANTED };
   }
@@ -294,13 +260,7 @@ class Twitter extends ApiBase {
     return { success: true, source, data: Twitter.parser(source, r.data) };
   }
 
-  async likes({
-    user_id,
-    since_id,
-    count = this.perpage,
-    max_id,
-    include_rts
-  } = {}) {
+  async likes({ user_id, since_id, count = this.perpage, max_id, include_rts } = {}) {
     const source = { name: "twitter", type: "likes", form: "listitems" };
     if (!this.granted) {
       return { success: false, source, data: this.messages.NOT_AUTHORIZED };
@@ -342,15 +302,11 @@ class Twitter extends ApiBase {
       data: {
         staticitems: {},
         listitems: [...d.timeline.data, ...d.likes.data].sort(
-          (a, b) => b.created_at - a.created_at
+          (a, b) => b.recreated_at - a.recreated_at
         )
       }
     };
   }
 }
 
-export default new Twitter(
-  TWITTER_USERNAME,
-  TWITTER_CONSUMER_KEY,
-  TWITTER_CONSUMER_SECRET
-);
+export default new Twitter(TWITTER_USERNAME, TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET);
